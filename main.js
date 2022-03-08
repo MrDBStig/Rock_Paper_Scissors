e7327a4972969df30179f96d75bb0780dd65df13
Mikita Kavaleuski <nikcrafter1999@gmail.com>
2022-03-08 20:30:24 +0300

    Link JS file to HTML, create computerPlay function

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const computerPlay = function () {
+  const choices = ["rock", "paper", "scissors"];
+  const randomChoiceKey = Math.floor(Math.random() * 3);
+  return choices[randomChoiceKey];
+};
+
+console.log(computerPlay());
